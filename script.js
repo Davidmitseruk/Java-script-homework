801e5197477211ed8209787842980bfdc432bdfa
Davidmitseruk <davidmitseruk@gmail.com>
2024-04-24 18:28:12 +0200

    Script js

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1
+const age = 14;
+console.log(age);
+
+// 2
+const myname = "David";
+console.log(myname);
+
+// 3
+const isStudent = true;
+console.log = "isStudent";
+
+//4
+const myString = "javascript";
+console.log(myString);
+
+//5
+const myNumber = 4;
+const a = myNumber + 10;
+console.log(b);
+
+//6
+const myNull = null;
+console.log(myNull);
+
+//7
+const user = prompt("Write your name");
+alert(`calling you, ${user}`);
+
+//8
+const userName = confirm("Do you accept this?");
+if (userName === true) {
+  alert("Thank you!");
+} else {
+  alert("Denied!");
+}
+
+//9
+alert("Not safe move!");
+const danger = confirm("this move is not safe, do you accept this?");
+if (danger === true) {
+  alert("Thank you!");
+} else {
+  alert("Denied!");
+}
